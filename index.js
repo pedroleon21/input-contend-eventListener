1e5f99e5e0b5cd5c4067fe857ee6a7b27213b15d
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2020-10-15 05:37:56 -0300

    remove comments

--- index.js
+++ index.js
@@ -6,13 +6,3 @@ input.addEventListener('input',updateValue);
 function updateValue(e){
     namePlace.textContent=e.target.value;
 }
-
-
-/* const input = document.querySelector('input');
-const log = document.getElementById('values');
-
-input.addEventListener('input', updateValue);
-
-function updateValue(e) {
-  log.textContent = e.target.value;
-} */
